599b409dc93d91dba5238ed40293a68c2b2d7a67
Katie Carrel <katiecarrel@comcast.net>
2020-11-20 13:05:23 -0800

    add index and show

--- app/controllers/grants_controller.rb
+++ app/controllers/grants_controller.rb
@@ -1,5 +1,13 @@
 class GrantsController < ApplicationController
 
+    def index 
+        @grants = Grant.all
+    end
+    
+    def show 
+        @grant = Grant.find(params[:id])
+    end
+
     def create 
         grant_params = params.require(:grant).permit(:researcher, :study_question, :start_date, :communication, :platform, :budget, :login_accounts, :funding_source, :population_size, :length_of_study, :computation, :third_party, :content, :HIPAA)
         Grant.create(grant_params)
